1647b97e4ec36c544cd4e7e2fedb462e30dce260
makaronma <mckahung250@gmail.com>
2022-04-03 17:53:20 +0800

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import { useEffect } from "react";
 
 const App = () => {
+  console.log('Fetch data in /api');
   useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
       .then((data) => console.log(data.message));
   }, []);
 
-  return <div className="App"></div>;
+  return <div className="App">App</div>;
 };
 
 export default App;
--- server.js
+++ server.js
@@ -1,28 +1,24 @@
+require("dotenv").config();
 const path = require("path");
 const express = require("express");
-
 const app = express();
+const cors = require("cors");
+app.use(cors());
 
-// Have Node serve the files for our built React app
-const PORT = process.env.PORT || 5000;
-
-// *** 
 // For Deployment
-// ***
-// app.use(express.static(path.resolve(__dirname, "client/build")));
+if (process.env.PORT) {
+  console.log("Production");
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  });
+}
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+  res.json({ message: "Hello World from server" });
 });
 
-// ***
-// For Deployment
-// ***
-// All other GET requests not handled before will return our React app
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
-// });
-
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
